e8a7ee87049e2e279696e43dd0adedfed9864f1b
tnfontenelle <tnfontenelle@hotmail.com>
2015-06-16 18:34:26 +0800

    Added about, help, contact us views

--- app.rb
+++ app.rb
@@ -7,4 +7,16 @@ class CarReviewAPI < Sinatra::Base
     haml :index
   end
 
+  get '/about' do
+    haml :about
+  end
+
+  get '/help' do
+    haml :help
+  end
+
+  get '/contact' do
+    haml :contact
+  end
+
 end
